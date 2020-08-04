99e22517da7d945198d446dad3d8d1a24a1dc443
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-08-04 17:14:27 +0200

    creation pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  get 'onherway', to: 'pages#onherway'
+
+
+
 end
