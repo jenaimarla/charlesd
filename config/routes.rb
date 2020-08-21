8aaadf41f507c690da0316bbf2e810194ac2ce78
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-08-21 21:38:29 +0200

    user access only edit ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'profile', to: 'pages#profile'
   resources :creations, only: [:index, :show, :edit, :update]
 
 end
