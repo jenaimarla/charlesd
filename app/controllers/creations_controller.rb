92ade304fa033736361ed49d2cc00928f6ae6593
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-09-01 17:36:07 +0200

    done

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -1,5 +1,5 @@
 class CreationsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @creations = Creation.all
